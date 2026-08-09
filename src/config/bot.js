1ef8dbb2e4a7391f8721b738d614c25caa9b4c29
radishcisneros-code <radish.cisneros@gmail.com>
2026-08-08 22:11:33 -0700

    Change crime cooldown calculation
    Updated crime cooldown value to a different calculation.

--- src/config/bot.js
+++ src/config/bot.js
@@ -189,7 +189,7 @@ export const botConfig = {
     cooldowns: {
       daily: 24 * 60 * 60 * 1000,
       work: 60 * 60 * 1000,
-      crime: 2 * 60 * 60 * 1000,
+      crime: 2 * 1 * 1 * 1,
       rob: 4 * 60 * 60 * 1000,
     },
 
