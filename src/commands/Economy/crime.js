d715472d71eac01ea9c5e0860c3f998f8a7d0dc2
radishcisneros-code <radish.cisneros@gmail.com>
2026-08-10 15:56:38 -0700

    Change currency name from coins to Burglebucks

--- src/commands/Economy/crime.js
+++ src/commands/Economy/crime.js
@@ -94,7 +94,7 @@ export default {
                 
                 const embed = successEmbed(
                     "🕵️ Crime Successful!",
-                    `You successfully committed ${crime.name} and earned **${amountEarned}** coins!`
+                    `You successfully committed ${crime.name} and earned **${amountEarned}** Burglebucks!`
                 );
                 
                 await InteractionHelper.safeEditReply(interaction, { embeds: [embed] });
